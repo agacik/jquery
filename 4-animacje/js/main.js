11bc856215670b3d5238e0810ef401af27ef6510
Agata Biesaga <abiesaga@gmail.com>
2018-04-29 14:19:59 +0200

    Ćwiczenie jQuery animacje.

--- 4-animacje/js/main.js
+++ 4-animacje/js/main.js
@@ -24,23 +24,4 @@ $(function() {
         .animate({'width': '100px','height': '100px'}, 2000, animatedDiv)
     }
 
-
-    
-//    $('p').eq(1).click(
-//        function () {
-//            $(this).css         
-//            ({'background-color': 'green',})
-//        });
-//    
-//    $('button').on( {
-//        'mouseenter': function (){
-//            $(this).css({'background-color': 'green'})
-//    },
-//        
-//    'mouseleave': function (){
-//        $(this).css({'background-color': 'blue'})
-//    }
-//  });
-    
-
 });
